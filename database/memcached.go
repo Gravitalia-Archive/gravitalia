d5fc67ab9c0555859c874f41676660e78d74c504
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-21 20:55:12 +0100

    Add Set function ✨

--- database/memcached.go
+++ database/memcached.go
@@ -3,3 +3,11 @@ package database
 import "github.com/bradfitz/gomemcache/memcache"
 
 var Mem = memcache.New("127.0.0.1:11211")
+
+func Set(key string, value string) {
+	Mem.Set(&memcache.Item{
+		Key:        key,
+		Value:      []byte(value),
+		Expiration: 300,
+	})
+}
