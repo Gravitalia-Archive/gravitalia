e77be648944af1a1aead8d541bed3b3f67c1bc9b
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-21 22:53:35 +0100

    Add documentation 📝

--- database/memcached.go
+++ database/memcached.go
@@ -1,9 +1,13 @@
 package database
 
-import "github.com/bradfitz/gomemcache/memcache"
+import (
+	"github.com/bradfitz/gomemcache/memcache"
+)
 
 var Mem = memcache.New("127.0.0.1:11211")
 
+// Set permits to set a temporary value (5 minutes), on the cache
+// via Memcached
 func Set(key string, value string) {
 	Mem.Set(&memcache.Item{
 		Key:        key,
