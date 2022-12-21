e7fa9b4a62a287b3b955f34ea1e1d0617a169e5d
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-21 20:55:29 +0100

    Remove useless comments 🔥

--- database/memgraph.go
+++ database/memgraph.go
@@ -6,16 +6,9 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
-type Item struct {
-	Message string
-}
-
-//var driver, _ = neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("", "", ""))
-var driver, err = neo4j.NewDriverWithContext("bolt://localhost:7687", neo4j.BasicAuth("", "", ""))
 var ctx = context.Background()
+var driver, _ = neo4j.NewDriverWithContext("bolt://localhost:7687", neo4j.BasicAuth("", "", ""))
 var Session = driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
-
-//var Session = driver.NewSession(neo4j.SessionConfig{})
 
 // CREATE (:User {vanity: "arianagrande"})-[:Subscribers]->(:User {vanity: "realhinome"})-[:Likes]->(:Post {id: "1055103553418567740"}); => Create user - ArianaGrande will be subscribed to RealHinome
 // MATCH (:User) -[:Subscribers]->(d:User) WHERE d.vanity = 'realhinome' RETURN count(*); => GET total followers
@@ -42,52 +35,3 @@ func CreateUser() (string, error) {
 
 	return "test", nil
 }
-
-/*func Createuser() (*Item, error) {
-	result, err := Session.WriteTransaction(createUser)
-	if err != nil {
-		return nil, err
-	}
-
-	return result.(*Item), nil
-}
-
-func createUser(tx neo4j.Transaction) (interface{}, error) {
-	records, err := tx.Run("CREATE (:User {vanity: 'arianagrande'})-[:Subscribers]->(:User {vanity: 'realhinome'})-[:Likes]->(:Post {id: '1055103553418567740'});", nil)
-	if err != nil {
-		return nil, err
-	}
-	record, err := records.Single()
-	if err != nil {
-		return nil, err
-	}
-	// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
-	return &Item{
-		Message: record.Values[0].(string),
-	}, nil
-}
-
-func GetUserFollowers() (*Item, error) {
-	result, err := Session.WriteTransaction(getFollower)
-	if err != nil {
-		return nil, err
-	}
-
-	return result.(*Item), nil
-}
-
-func getFollower(tx neo4j.Transaction) (interface{}, error) {
-	records, err := tx.Run("MATCH (:User) -[:Likes]->(:Post) -[:Subs]->(d:User) WHERE d.name = 'Alice' RETURN count(*);", nil)
-	if err != nil {
-		return nil, err
-	}
-	record, err := records.Single()
-	if err != nil {
-		return nil, err
-	}
-	// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
-	return &Item{
-		Message: record.Values[0].(string),
-	}, nil
-}
-*/
