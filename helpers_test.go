442e469cda1d9fcd543482e04bc2d1f583ecc3a1
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-26 19:32:04 +0100

    Update test ✅

--- helpers_test.go
+++ helpers_test.go
@@ -13,7 +13,7 @@ func TestCreateToken(t *testing.T) {
 		t.Fatalf(`CreateToken("test") = %q, got an error`, err)
 	}
 
-	jwtCheck := regexp.MustCompile(`[A-z|0-9]{48}/{2}[A-z|0-9]{1,}`)
+	jwtCheck := regexp.MustCompile(`(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)`)
 	if !jwtCheck.MatchString(jwt) {
 		t.Fatalf(`CreateToken("test") = %q, want match for %#q, nil`, jwt, jwtCheck)
 	}
