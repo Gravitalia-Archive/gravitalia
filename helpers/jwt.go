081a42a12411422c4132ad3d0a2fceb18e498444
Hinome <57831472+RealHinome@users.noreply.github.com>
2022-12-26 19:06:58 +0100

    Add check token ✨

--- helpers/jwt.go
+++ helpers/jwt.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"encoding/json"
+	"errors"
 	"os"
 	"time"
 
@@ -27,4 +29,40 @@ func CreateToken(vanity string) (string, error) {
 	}
 
 	return token.String(), nil
+}
+
+func CheckToken(token string) (string, error) {
+	verifier, err := jwt.NewVerifierHS(jwt.HS512, []byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+
+	tokenBytes := []byte(token)
+	newToken, err := jwt.Parse(tokenBytes, verifier)
+	if err != nil {
+		return "", err
+	}
+
+	err = verifier.Verify(newToken)
+	if err != nil {
+		return "", err
+	}
+
+	// get Registered claims
+	var newClaims jwt.RegisteredClaims
+	err = json.Unmarshal(newToken.Claims(), &newClaims)
+	if err != nil {
+		return "", err
+	}
+
+	err = jwt.ParseClaims(tokenBytes, verifier, &newClaims)
+	if err != nil {
+		return "", err
+	}
+
+	if !newClaims.IsValidAt(time.Now()) {
+		return "", errors.New("invalid time")
+	}
+
+	return newClaims.Subject, nil
 }
