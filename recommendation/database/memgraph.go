d29866bfdad6880d16f553ad57f5434a7c425fb8
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-21 19:11:33 +0200

    Add new function

--- recommendation/database/memgraph.go
+++ recommendation/database/memgraph.go
@@ -61,7 +61,7 @@ func LastFollowingPost(id string) ([]model.Post, error) {
 
 	_, err := Session.ExecuteWrite(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
 		result, err := transaction.Run(ctx,
-			"MATCH (n:User) -[:Subscriber]->(u:User) WHERE n.name = $id WITH n, u MATCH (u:User) -[:Create]->(p:Post) WHERE not exists((n)-[:View]->(p)) RETURN p ORDER BY p.id, p.description, p.text LIMIT 20;",
+			"MATCH (n:User {name: 'realhinome'})-[:Subscriber]->(u:User) MATCH (u)-[:Create]->(p:Post)-[:Show]->(t:Tag) WHERE NOT EXISTS((n)-[:View]->(p)) WITH p, t ORDER BY p.id DESC LIMIT 20 RETURN p.id, p.description, p.text, t.name;",
 			map[string]any{"id": id})
 		if err != nil {
 			return nil, err
@@ -79,6 +79,85 @@ func LastFollowingPost(id string) ([]model.Post, error) {
 			list[pos].Id = record.Values[0].(string)
 			list[pos].Description = record.Values[1].(string)
 			list[pos].Text = record.Values[2].(string)
+			list[pos].Tag = record.Values[3].(string)
+
+			pos++
+		}
+
+		return list, nil
+	})
+	if err != nil {
+		return list, err
+	}
+
+	return list, nil
+}
+
+// LastCommunityPost allows to find the last n publications
+// posted by the same community as the user
+func LastCommunityPost(id string) ([]model.Post, error) {
+	list := make([]model.Post, 0)
+
+	_, err := Session.ExecuteWrite(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
+		result, err := transaction.Run(ctx,
+			"MATCH (u:User {name: $id}) WITH u.community as community MATCH (a)-[r]->(p:Post)-[:Show]->(t:Tag) WHERE a.community = community WITH p, t, count(r) as connections ORDER BY connections DESC LIMIT 100 WITH p, t ORDER BY p.id DESC LIMIT 30 RETURN p.id, p.text, p.description, t.name;",
+			map[string]any{"id": id})
+		if err != nil {
+			return nil, err
+		}
+
+		pos := 0
+		for result.Next(ctx) {
+			if result.Record().Values[0] == nil {
+				return list, nil
+			}
+
+			record := result.Record()
+			list = append(list, model.Post{})
+
+			list[pos].Id = record.Values[0].(string)
+			list[pos].Text = record.Values[1].(string)
+			list[pos].Description = record.Values[2].(string)
+			list[pos].Tag = record.Values[3].(string)
+
+			pos++
+		}
+
+		return list, nil
+	})
+	if err != nil {
+		return list, err
+	}
+
+	return list, nil
+}
+
+// LastLikedPost allows access to the last posts
+// made with the same tag as the last liked post
+func LastLikedPost(id string) ([]model.Post, error) {
+	list := make([]model.Post, 0)
+
+	_, err := Session.ExecuteWrite(ctx, func(transaction neo4j.ManagedTransaction) (any, error) {
+		result, err := transaction.Run(ctx,
+			"MATCH (:User {name: 'realhinome'})-[:Like]->(p:Post)-[:Show]->(t:Tag) WITH p, t ORDER BY p.id DESC LIMIT 1 WITH t MATCH (p:Post)-[:Show]->(t:Tag) WITH p, t ORDER BY p.id DESC LIMIT 10 RETURN p.id, p.text, p.description, t.name;",
+			map[string]any{"id": id})
+		if err != nil {
+			return nil, err
+		}
+
+		pos := 0
+		for result.Next(ctx) {
+			if result.Record().Values[0] == nil {
+				return list, nil
+			}
+
+			record := result.Record()
+			list = append(list, model.Post{})
+
+			list[pos].Id = record.Values[0].(string)
+			list[pos].Text = record.Values[1].(string)
+			list[pos].Description = record.Values[2].(string)
+			list[pos].Tag = record.Values[3].(string)
 
 			pos++
 		}
