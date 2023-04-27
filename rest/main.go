00cbded5e5dd49cc1543c89ea4be5522a16f8ef0
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-27 15:57:19 +0200

    Add metrics route

--- rest/main.go
+++ rest/main.go
@@ -10,11 +10,21 @@ import (
 	"github.com/Gravitalia/gravitalia/helpers"
 	"github.com/Gravitalia/gravitalia/router"
 	"github.com/joho/godotenv"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	// Get key-value in .env file
 	godotenv.Load()
+
+	// Prometheus
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(
+		collectors.NewGoCollector(),
+		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
+	)
 
 	// Create routes
 	http.HandleFunc("/", router.Index)
@@ -23,6 +33,13 @@ func main() {
 	http.HandleFunc("/users/", router.Users)
 	http.HandleFunc("/relation/", router.Relation)
 	http.HandleFunc("/posts/", router.Post)
+	http.Handle("/metrics",
+		helpers.New(
+			registry, nil).
+			WrapHandler("/metrics", promhttp.HandlerFor(
+				registry,
+				promhttp.HandlerOpts{}),
+			))
 
 	// Init every helpers function
 	helpers.Init()
