fdad2d29667908e2d7b0f4b842d5103f8486d390
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-04-27 21:15:36 +0200

    Update metrics

--- rest/main.go
+++ rest/main.go
@@ -1,30 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"log"
-
 	"github.com/Gravitalia/gravitalia/helpers"
 	"github.com/Gravitalia/gravitalia/router"
 	"github.com/joho/godotenv"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	// Get key-value in .env file
 	godotenv.Load()
-
-	// Prometheus
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(
-		collectors.NewGoCollector(),
-		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
-	)
 
 	// Create routes
 	http.HandleFunc("/", router.Index)
@@ -33,14 +23,7 @@ func main() {
 	http.HandleFunc("/users/", router.Users)
 	http.HandleFunc("/relation/", router.Relation)
 	http.HandleFunc("/posts/", router.Post)
-	http.Handle(
-		"/metrics",
-		helpers.New(
-			registry, nil).
-			WrapHandler("/metrics", promhttp.HandlerFor(
-				registry,
-				promhttp.HandlerOpts{}),
-			))
+	http.Handle("/metrics", promhttp.HandlerFor(helpers.GetRegistery(), promhttp.HandlerOpts{}))
 
 	// Init every helpers function
 	helpers.Init()
