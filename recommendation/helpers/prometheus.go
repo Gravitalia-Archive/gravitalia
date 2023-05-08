49e138e376cca3333716ab8e7fc7fea567a32ec5
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-05-08 19:33:53 +0200

    Add comments

--- recommendation/helpers/prometheus.go
+++ recommendation/helpers/prometheus.go
@@ -19,6 +19,8 @@ var (
 	})
 )
 
+// GetRegistery is used to get prometheus
+// saved data
 func GetRegistery() *prometheus.Registry {
 	registry := prometheus.NewRegistry()
 	registry.MustRegister(
@@ -31,10 +33,14 @@ func GetRegistery() *prometheus.Registry {
 	return registry
 }
 
+// IncrementRequests allows to increment
+// the number of total requests
 func IncrementRequests() {
 	requestsTotal.Inc()
 }
 
+// ObserveRequestDuration allows to create a
+// new record of time duration
 func ObserveRequestDuration(time float64) {
 	requestDuration.Observe(time)
 }
