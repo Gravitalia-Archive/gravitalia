e45020671f2dea4f828052d94203e5a924c310b9
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-16 23:25:50 +0200

    Update structs

---
+++ rest/model/Post.go
@@ -0,0 +1,18 @@
+package model
+
+// Post struct defines how post must be
+type Post struct {
+	Id          string `json:"id"`
+	Description string `json:"description"`
+	Text        string `json:"text"`
+	Like        int64  `json:"like"`
+	Author      string `json:"author"`
+	Comments    []any  `json:"comments,omitempty"`
+}
+
+// PostBody defines how body when posting
+// new image must be
+type PostBody struct {
+	Description string   `json:"description"`
+	Images      [][]byte `json:"images"`
+}
