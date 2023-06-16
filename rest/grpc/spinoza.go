c9d5413a14dc63725afd7695d4425378eadab85c
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-16 23:24:08 +0200

    Add grpc clients

---
+++ rest/grpc/spinoza.go
@@ -0,0 +1,41 @@
+package grpc
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/Gravitalia/gravitalia/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// UploadImage allows to transfer image as bytes
+// into Spinoza server to upload it to image provider
+func UploadImage(image []byte) (string, error) {
+	// Set up a connection to the server
+	conn, err := grpc.Dial(os.Getenv("SPINOZA_ADDRESS"), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return "", err
+	}
+
+	defer conn.Close()
+	c := proto.NewSpinozaClient(conn)
+
+	// Contact the server and print out its response
+	// If no response in 5 seconds, cancel it
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Make request
+	r, err := c.Upload(ctx, &proto.UploadRequest{
+		Data:  image,
+		Width: 3840, // 1920 for FHD
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return r.GetMessage(), nil
+}
