4c5c60ede34de05eb440d05fb666cbe5a90fc8a3
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-06-18 16:48:21 +0200

    Add models

---
+++ search/src/model.rs
@@ -0,0 +1,14 @@
+use serde::{Serialize, Deserialize};
+
+/// Error struct defines how message with
+/// error must be
+#[derive(Serialize)]
+pub struct Error {
+    pub error: bool,
+    pub message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    vanity: String
+}
