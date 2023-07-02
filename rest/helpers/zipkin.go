a5f39a36e53948bee10babcd0e8941d498509b63
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-02 14:54:52 +0200

    Add zipkin tracer support

---
+++ rest/helpers/zipkin.go
@@ -0,0 +1,44 @@
+package helpers
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
+	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
+)
+
+func InitTracer() (*zipkinhttp.Client, func(http.Handler) http.Handler) {
+	// set up a span reporter
+	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
+	defer func() {
+		_ = reporter.Close()
+	}()
+
+	// create our local service endpoint
+	endpoint, err := zipkin.NewEndpoint("gravitaliaRest", "localhost:"+os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	// initialize our tracer
+	tracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		log.Fatalf("unable to create tracer: %+v\n", err)
+	}
+
+	// create global zipkin http server middleware
+	serverMiddleware := zipkinhttp.NewServerMiddleware(
+		tracer, zipkinhttp.TagResponseSize(true),
+	)
+
+	// create global zipkin traced http client
+	client, err := zipkinhttp.NewClient(tracer, zipkinhttp.ClientTrace(true))
+	if err != nil {
+		log.Fatalf("unable to create client: %+v\n", err)
+	}
+
+	return client, serverMiddleware
+}
