67b1965230ffc734efa350bdfa588ddf2e29e51d
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-02 20:19:05 +0200

    Use QuerySearch instead of string

--- search/src/router/research.rs
+++ search/src/router/research.rs
@@ -1,11 +1,12 @@
 use warp::{reply::{WithStatus, Json}, http::StatusCode};
 use crate::database;
 use anyhow::Result;
+use crate::model;
 
 /// This route allows to search in all documents
-pub async fn research(query: String) -> Result<WithStatus<Json>> {
-    if query == "*" {
-        return Ok(warp::reply::with_status(warp::reply::json(&crate::model::Error {
+pub async fn research(query: model::QuerySearch) -> Result<WithStatus<Json>> {
+    if query.q == "*" {
+        return Ok(warp::reply::with_status(warp::reply::json(&model::Error {
             error: true,
             message: "Cannot search '*'".to_string(),
         }), StatusCode::BAD_REQUEST));
@@ -13,7 +14,7 @@ pub async fn research(query: String) -> Result<WithStatus<Json>> {
 
     Ok(warp::reply::with_status(
         warp::reply::json(
-            &database::search(query)
+            &database::search(query.q, query.limit.unwrap_or_else(|| 3).max(20))
                 .await?
                 .hits
                 .into_iter()
