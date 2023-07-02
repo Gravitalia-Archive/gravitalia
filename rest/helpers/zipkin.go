77377f11d9df187d8ec8f4c63a9aa54f1708d9cc
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-02 16:07:50 +0200

    Use HTTP reporter

--- rest/helpers/zipkin.go
+++ rest/helpers/zipkin.go
@@ -7,18 +7,18 @@ import (
 
 	"github.com/openzipkin/zipkin-go"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
-	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
+	httpreporter "github.com/openzipkin/zipkin-go/reporter/http"
 )
 
 func InitTracer() (*zipkinhttp.Client, func(http.Handler) http.Handler) {
 	// set up a span reporter
-	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
+	reporter := httpreporter.NewReporter("http://" + os.Getenv("ZIPKIN_ADDRESS") + "/api/v2/spans")
 	defer func() {
 		_ = reporter.Close()
 	}()
 
 	// create our local service endpoint
-	endpoint, err := zipkin.NewEndpoint("gravitaliaRest", os.Getenv("ZIPKIN_ADDRESS"))
+	endpoint, err := zipkin.NewEndpoint("gravitaliaRest", "localhost:"+os.Getenv("PORT"))
 	if err != nil {
 		log.Printf("unable to create local endpoint: %+v\n", err)
 	}
