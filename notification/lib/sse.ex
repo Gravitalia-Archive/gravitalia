625ec7b60470fff6104a2c47b9b30985cbe18f46
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-10 01:51:57 +0200

    Check user authentification

--- notification/lib/sse.ex
+++ notification/lib/sse.ex
@@ -11,25 +11,49 @@ defmodule Notification.SSE do
   def call(conn, _opts) do
     conn =
       conn
-      |> put_resp_header("Access-Control-Allow-Origin", "*")
+      |> put_resp_header("Access-Control-Allow-Origin", "https://www.gravitalia.com")
+      |> put_resp_header("Access-Control-Allow-Credentials", "true")
       |> put_resp_header("Access-Control-Allow-Methods", "GET")
       |> put_resp_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
       |> put_resp_header("Access-Control-Max-Age", "3600")
       |> put_resp_header("Cache-Control", "no-cache")
       |> put_resp_header("connection", "keep-alive")
-      |> put_resp_header("Content-Type", "text/event-stream; charset=utf-8")
-      |> send_chunked(200)
 
-    PubSub.subscribe(self(), "user_id")
+    case get_token(conn) do
+      nil ->
+        unauthorized(conn)
 
-    sse_loop(conn, self())
+      token ->
+        case Notification.Helpers.check_token(token) do
+          nil ->
+            unauthorized(conn)
+
+          user_id ->
+            conn =
+              conn
+              |> put_resp_header("Content-Type", "text/event-stream; charset=utf-8")
+              |> send_chunked(200)
+
+            PubSub.subscribe(self(), user_id)
+            sse_loop(conn, self())
+        end
+    end
+  end
+
+  defp unauthorized(conn) do
+    conn =
+      conn
+      |> put_resp_header("Content-Type", "application/json")
+      |> send_chunked(401)
+      |> chunk(Jason.encode!(%{error: true, message: "Invalid token"}))
+
+    conn
   end
 
   defp sse_loop(conn, pid) do
     receive do
       {message} ->
-        chunk(conn, "id:#{UUID.uuid1()}\nevent: message\ndata: #{Jason.encode!(message)}\n\n")
-
+        chunk(conn, format_sse_message(message))
         sse_loop(conn, pid)
 
       {:DOWN, _reference, :process, ^pid, _type} ->
@@ -37,6 +61,23 @@ defmodule Notification.SSE do
 
       _other ->
         sse_loop(conn, pid)
+    end
+  end
+
+  defp format_sse_message(message) do
+    "id: #{UUID.uuid1()}\nevent: message\ndata: #{Jason.encode!(message)}\n\n"
+  end
+
+  defp get_token(conn) do
+    case List.first(get_req_header(conn, "cookie")) do
+      nil ->
+        nil
+
+      cookie_string ->
+        case Regex.run(~r/token=([^;]+)/, cookie_string) do
+          [_, token] -> token
+          _ -> nil
+        end
     end
   end
 end
