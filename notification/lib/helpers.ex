f6c8d9e6eda998096b8ec97103e6ca247fe86052
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-09 20:01:02 +0200

    Add new helper function

---
+++ notification/lib/helpers.ex
@@ -0,0 +1,24 @@
+defmodule Notification.Helpers do
+  @moduledoc """
+  Helpers are a set of functions designed for use in the program
+  """
+
+  def check_token(token) do
+    {ok, claims} =
+      Joken.Signer.verify(
+        token,
+        Joken.Signer.create("RS256", %{"pem" => Application.get_env(:notification, :jwt)})
+      )
+
+    cond do
+      ok != :ok ->
+        nil
+
+      :os.system_time(:millisecond) / 1000 >= Map.get(claims, "exp") ->
+        nil
+
+      true ->
+        Map.get(claims, "sub")
+    end
+  end
+end
