d40b5dde293592d0b612dfb3626a46e2b5c48218
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-18 23:17:06 +0200

    Log error

--- rest/helpers/nats.go
+++ rest/helpers/nats.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"log"
 	"os"
 
 	"github.com/nats-io/nats.go"
@@ -8,10 +9,16 @@ import (
 
 var Nats *nats.Conn
 
+// InitNATS starts a new NATS instance
 func InitNATS() {
 	Nats, _ = nats.Connect(os.Getenv("NATS_URL"))
 }
 
+// Publish allows publishing message on NATS
 func Publish(subject string, message []byte) {
-	Nats.Publish(subject, message)
+	err := Nats.Publish(subject, message)
+
+	if err != nil {
+		log.Printf("(Publish) Failed to send message to %v, got error: %v", subject, err)
+	}
 }
