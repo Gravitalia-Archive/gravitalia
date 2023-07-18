ada02060d37ad7829221ae6ff5bd101ac126c2b7
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-07-18 23:05:52 +0200

    Add comments

--- rest/model/Nats.go
+++ rest/model/Nats.go
@@ -3,8 +3,11 @@ package model
 // Message represents how NATS publish message
 // should be
 type Message struct {
-	Type      string `json:"type"`
-	From      string `json:"from"`
-	To        string `json:"string"`
-	Important bool   `json:"important"`
+	Type string `json:"type"`
+	// Author vanity (such as realhinome)
+	From string `json:"from"`
+	// Must be User vanity or post ID
+	To string `json:"string"`
+	// Set true to send push notification
+	Important bool `json:"important"`
 }
